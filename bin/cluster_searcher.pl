f44982b3afaeef1ac69700e7d364dcb0a245acdc
Dan Markham <dmarkham@eventful.com>
2011-11-25 23:22:43 -0800

    working without ev

--- bin/cluster_searcher.pl
+++ bin/cluster_searcher.pl
@@ -1,15 +1,17 @@
 use strict;
 use warnings;
+
 use Lucy;
 use Getopt::Long;
-use Data::MessagePack;
 use ZeroMQ::Raw;
 use ZeroMQ::Constants qw/:all/;
-use AnyEvent;
-use AnyEvent::Loop;
+
 use Storable qw( nfreeze thaw );
+use Data::MessagePack;
 use File::Slurp;
 use Data::Dumper;
+
+
 
 ##  This Script is a Dumb generic Searcher it can search any index
 ##  in the index_dir you tell this script who to connect up to
@@ -19,7 +21,6 @@ use Data::Dumper;
 ## ping  node (it's boss) with a heartbeat
 ## node askes searcher about what he has
 ## node starts sending querys to this searcher (that he can help on)
-##
 
 ## This is who I will be getting my work from 
 my $my_node_hostport = "127.0.0.1:9905";
@@ -40,54 +41,41 @@ my $debug = 0;
 
 ## our connection to the Node
 my $context   = zmq_init();
-my $requester = zmq_socket( $context, ZMQ_REQ);
-zmq_connect( $requester, 'tcp://' . $my_node_hostport );
-
-
-## i can only do 1 query at a time.
-## but if the server doesnt finish a query 
-## i'll need a flag and a way to clean up.
-## this will also help me cordinate a reopen
-## while not being in then middle of a search
+my $requester;
 
 my %global_state;
-$global_state{last_seen_node} = time - 60;
+$global_state{last_seen_node} = 0;
 
 my $rand_id = int(rand(1_000_000));
 
-## every 5 seconds check our node
-my $heartbeat = AE::timer 10, 5, sub { 
-      print "Checking Heartbeat\n";
-      if(time - $global_state{last_seen_node} > 20){
-        print "Sent Hello\n";
-        $global_state{last_seen_node} = time;
-        zmq_close($requester);
-        $requester = zmq_socket( $context, ZMQ_REQ);
-        zmq_connect( $requester, 'tcp://' . $my_node_hostport );
-        send_data($requester,{_action => 'hello'});
-      }
-      else{
-        print "Still chatting\n";
-      }
-    }; 
+while(1){
 
+  print "Checking Heartbeat on $my_node_hostport\n";
+  if(time - $global_state{last_seen_node} > 20){
+    print "Sent Hello to node\n" if $debug;
+    $global_state{last_seen_node} = time;
+    zmq_close($requester) if $requester;
+    $requester = zmq_socket( $context, ZMQ_REQ);
+    zmq_connect( $requester, 'tcp://' . $my_node_hostport );
+    send_data($requester,{_action => 'hello'});
+  }
 
-my $requester_fh = zmq_getsockopt( $requester, ZMQ_FD );
-my $w; $w = AE::io $requester_fh, 1, sub {
-        while ( my $msg = zmq_recv( $requester, ZMQ_RCVMORE ) ) {
-          $global_state{last_seen_node} = time;
-          my $data  = zmq_msg_data($msg);
-          my $resp = dispatch($data);
-          send_data($requester, $resp);
-        }
-    };
+  zmq_poll([
+            {
+              socket => $requester,
+              events => ZMQ_POLLIN,
+              callback => sub {
+                while ( my $msg = zmq_recv( $requester, ZMQ_RCVMORE ) ) {
+                  $global_state{last_seen_node} = time;
+                  my $data  = zmq_msg_data($msg);
+                  my $resp = dispatch($data);
+                  send_data($requester, $resp);
+                }
+              },
+            } 
+        ], 5_000_000);
+}
 
-
-## this kicks off eveything i have to ping the node first!
-## after i say hello he will ask me to do work for him
-## after he figures out what skills and data i have
-send_data($requester,{_action => 'hello'});
-AnyEvent::Loop::run;
 zmq_close($requester);
 
 exit;
@@ -108,20 +96,19 @@ sub dispatch{
   my $data;
   eval{ $data = $mp->unpack($arg); };
   return {status => 'error'} unless $data;
-  print Dumper($data);
+  print "Recived: " . Dumper($data) if $debug;
   my $method = delete $data->{_action};
 
   if($method eq 'index_status'){
     ## read a json file from disk some other system keeping
     ## up to date and send it. This file will let the Node server
-    ## know what index/shards  i have
+    ## know what index/shards i have
     my $utf_text = "";
     eval{$utf_text= read_file( "$index_dir/index_list.json", binmode => ':utf8' )} ;
     return {index_status => $utf_text };
   }
   
   my $searcher = Lucy::Search::IndexSearcher->new( index => '/path/to/index' );
-
   
   if($allowed_lucy_methods{$method}){
     my $response   = $searcher->$method();
@@ -131,16 +118,12 @@ sub dispatch{
 }
 
 
-
-
 sub send_data{
   my($socket,$data)  = @_;
   $data->{_worker_id} = $$ . "_$rand_id";
-  my $rv = eval {
+  return  eval {
       my $msg  = zmq_msg_init_data( $mp->pack($data) );
       return  zmq_send( $requester, $msg );
     };
-    
-  return $rv;
 }
 
