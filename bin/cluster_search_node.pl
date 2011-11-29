2abb0012a6326985c223679a891b2ba760c6fd6e
Dan Markham <dmarkham@gmail.com>
2011-11-28 23:16:03 -0800

    checkpoint

--- bin/cluster_search_node.pl
+++ bin/cluster_search_node.pl
@@ -4,6 +4,7 @@ use Lucy;
 use ClusterNode;
 use Data::Dumper;
 
+use Getopt::Long;
 
 ## the cluster Nodes flow..
 ## set up a listener that the cluster_searchers will be pinging.
@@ -12,7 +13,27 @@ use Data::Dumper;
 ##
 ## Fanout clients requests to the workers
 
-my $cn = ClusterNode->new( hostport => '127.0.0.1:9905');
+
+
+my $debug = 0;
+my $hostport = '127.0.0.1:9905';
+my $upstream_host;
+&GetOptions(
+   'debug' => \$debug, 
+   'hostport' => \$hostport, 
+   'upstream_host' => \$upstream_host, 
+   'index_dir' => \$index_dir, 
+);
+
+
+
+## Listen for my workers/child nodes
+my $cn = ClusterNode->new( hostport => $hostport);
+
+if($upstream_host){
+  
+
+}
 
 
 
